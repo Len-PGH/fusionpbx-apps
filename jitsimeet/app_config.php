e78d9cb019c0f8d81fcde74bf35ade9c2fb7f40c
Len <Len-PGH@users.noreply.github.com>
2020-10-26 23:57:13 -0400

    Update app_config.php

--- jitsimeet/app_config.php
+++ jitsimeet/app_config.php
@@ -2,12 +2,12 @@
 	//application details
 		$apps[$x]['name'] = 'JitsiMeet';
 		$apps[$x]['uuid'] = '0e436409-1f0c-4e03-9af8-b8dcc4d1a0c0';
-		$apps[$x]['category'] = 'JitsiMeet';
-		$apps[$x]['subcategory'] = 'JitsiMeet';
+		$apps[$x]['category'] = '';
+		$apps[$x]['subcategory'] = '';
 		$apps[$x]['version'] = '1.0';
 		$apps[$x]['license'] = 'Mozilla Public License 1.1';
 		$apps[$x]['url'] = 'http://www.fusionpbx.com';
-		$apps[$x]['description']['en-us'] = 'Create a room on your Jitsi-Meet server and set the room name here.';
+		$apps[$x]['description']['en-us'] = 'Create a room on your Jitsi-Meet server then set the domain and room name here.';
 
 	//permission details
 		$y = 0;
@@ -18,45 +18,37 @@
 		$y++;
 
 	//schema details
-		$y = 0; //table array index
-		$z = 0; //field array index
-		$apps[$x]['db'][$y]['table'] = 'v_jitsimeet';
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'domain_uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_domains';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'domain_uuid';
-		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'jitsimeet_uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'primary';
-		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'jitsimeet_name';
-		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enter the room name.';
-		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'jitsimeet_domain';
-		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'Enter the domain name of the Jitsi-meet server.';
-		$z++;
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'jitsimeet_description';
-		$apps[$x]['db'][$y]['fields'][$z]['type'] = 'text';
-		$apps[$x]['db'][$y]['fields'][$z]['description']['en-us'] = 'An easy way to embed Jitsi-meet on a FusionPBX installation.';
-		$z++;
+	//	$y = 0; //table array index
+	//	$z = 0; //field array index
+	//	$apps[$x]['db'][$y]['table'] = 'v_jitsimeet';
+	//	$apps[$x]['db'][$y]['fields'][$z]['name'] = 'domain_uuid';
+	//	$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
+	//	$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
+	//	$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
+	//	$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
+	//	$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_domains';
+	//	$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'domain_uuid';
+	//	$z++;
+		
 
-		$y = 1; //table array index
-		$z = 0; //field array index
-		$apps[$x]['db'][$y]['table'] = 'v_jitsimeet';
-		$apps[$x]['db'][$y]['fields'][$z]['name'] = 'domain_uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['pgsql'] = 'uuid';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['sqlite'] = 'text';
-		$apps[$x]['db'][$y]['fields'][$z]['type']['mysql'] = 'char(36)';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['type'] = 'foreign';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['table'] = 'v_domains';
-		$apps[$x]['db'][$y]['fields'][$z]['key']['reference']['field'] = 'domain_uuid';
-		$z++;
+	//default settings
+		$y=0;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "32da4a78-ea1e-41dc-9bf7-562acb0efbbc";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "jitsimeet";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "jitsimeet_name";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "test";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Enter the room name.";
+		$y++;
+		$apps[$x]['default_settings'][$y]['default_setting_uuid'] = "606fd2b5-26fb-46ca-9751-9c33ae89641e";
+		$apps[$x]['default_settings'][$y]['default_setting_category'] = "jitsimeet";
+		$apps[$x]['default_settings'][$y]['default_setting_subcategory'] = "jitsimeet_domain";
+		$apps[$x]['default_settings'][$y]['default_setting_name'] = "text";
+		$apps[$x]['default_settings'][$y]['default_setting_value'] = "sub.domain.tld";
+		$apps[$x]['default_settings'][$y]['default_setting_enabled'] = "true";
+		$apps[$x]['default_settings'][$y]['default_setting_description'] = "Enter the domain name of the Jitsi-meet server.";
+		$y++;
+
+
 ?>
