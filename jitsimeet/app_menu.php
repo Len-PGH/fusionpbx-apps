771dc32a813cc792052aa4598baed66187b96d1b
Len <Len-PGH@users.noreply.github.com>
2020-10-27 11:54:35 -0400

    Update app_menu.php

--- jitsimeet/app_menu.php
+++ jitsimeet/app_menu.php
@@ -5,6 +5,9 @@
 	$apps[$x]['menu'][0]['parent_uuid'] = 'cd544c95-921a-4386-94d9-81913bd8e949';
 	$apps[$x]['menu'][0]['category'] = 'internal';
 	$apps[$x]['menu'][0]['path'] = '/app/jitsimeet/jitsimeet.php';
+	$apps[$x]['menu'][$y]['icon'] = "fa-yin-yang";
+	$apps[$x]['menu'][$y]['order'] = "50";
 	$apps[$x]['menu'][0]['groups'][] = 'superadmin';
+	$apps[$x]['menu'][0]['groups'][] = 'admin';
 
 ?>
